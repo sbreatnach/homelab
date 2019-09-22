43addf9f0584acba1d3e874481d16eb411071c45
Shane Breatnach <shane.breatnach@gmail.com>
2019-09-22 20:57:15 +0100

    New containers for running DNS queries through Adguard with custom nameserver support.

--- build_images.sh
+++ build_images.sh
@@ -2,8 +2,16 @@
 
 ROMPR_VERSION=1.31
 MOPIDY_VERSION=2.2.3
+SNAPCAST_VERSION=0.15.0
 
 docker build --build-arg ROMPR_VERSION=${ROMPR_VERSION} \
-    -t sbreatnach/rmprdckr:${ROMPR_VERSION} ./rompr
+    -t registry.nas.home/sbreatnach/rmprdckr:${ROMPR_VERSION} ./rompr
+docker push registry.nas.home/sbreatnach/rmprdckr:${ROMPR_VERSION}
+
 docker build --build-arg MOPIDY_VERSION=${MOPIDY_VERSION} \
-    -t sbreatnach/mopidy:${MOPIDY_VERSION} ./mopidy
+    -t registry.nas.home/sbreatnach/mopidy:${MOPIDY_VERSION} ./mopidy
+docker push registry.nas.home/sbreatnach/mopidy:${MOPIDY_VERSION}
+
+docker build --build-arg SNAPCAST_VERSION=${SNAPCAST_VERSION} \
+    -t registry.nas.home/sbreatnach/snapcast:${SNAPCAST_VERSION} ./snapcast
+docker push registry.nas.home/sbreatnach/snapcast:${SNAPCAST_VERSION}
