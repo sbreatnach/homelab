81a0ce8cb2e3e59daf431f2e5bafc9be14ddca7a
Shane Breatnach <shane.breatnach@gmail.com>
2019-09-23 18:20:24 +0100

    Final touches to working version

--- build_images.sh
+++ build_images.sh
@@ -5,13 +5,13 @@ MOPIDY_VERSION=2.2.3
 SNAPCAST_VERSION=0.15.0
 
 docker build --build-arg ROMPR_VERSION=${ROMPR_VERSION} \
-    -t registry.nas.home/sbreatnach/rmprdckr:${ROMPR_VERSION} ./rompr
-docker push registry.nas.home/sbreatnach/rmprdckr:${ROMPR_VERSION}
+    -t registry.nas.home/rmprdckr:${ROMPR_VERSION} ./rompr
+docker push registry.nas.home/rmprdckr:${ROMPR_VERSION}
 
 docker build --build-arg MOPIDY_VERSION=${MOPIDY_VERSION} \
-    -t registry.nas.home/sbreatnach/mopidy:${MOPIDY_VERSION} ./mopidy
-docker push registry.nas.home/sbreatnach/mopidy:${MOPIDY_VERSION}
+    -t registry.nas.home/mopidy:${MOPIDY_VERSION} ./mopidy
+docker push registry.nas.home/mopidy:${MOPIDY_VERSION}
 
 docker build --build-arg SNAPCAST_VERSION=${SNAPCAST_VERSION} \
-    -t registry.nas.home/sbreatnach/snapcast:${SNAPCAST_VERSION} ./snapcast
-docker push registry.nas.home/sbreatnach/snapcast:${SNAPCAST_VERSION}
+    -t registry.nas.home/snapcast:${SNAPCAST_VERSION} ./snapcast
+docker push registry.nas.home/snapcast:${SNAPCAST_VERSION}
