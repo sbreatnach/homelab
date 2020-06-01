be4a2face50366ce4394db60cc0f5dd7f13b6571
Shane Breatnach <shane.breatnach@gmail.com>
2020-06-01 11:59:06 +0100

    Removed unused images. Added subsonic

--- build_images.sh
+++ build_images.sh
@@ -4,10 +4,6 @@ ROMPR_VERSION=1.31
 MOPIDY_VERSION=2.2.3
 SNAPCAST_VERSION=0.15.0
 
-docker build --build-arg ROMPR_VERSION=${ROMPR_VERSION} \
-    -t registry.nas.home/rmprdckr:${ROMPR_VERSION} ./rompr
-docker push registry.nas.home/rmprdckr:${ROMPR_VERSION}
-
 docker build --build-arg MOPIDY_VERSION=${MOPIDY_VERSION} \
     -t registry.nas.home/mopidy:${MOPIDY_VERSION} ./mopidy
 docker push registry.nas.home/mopidy:${MOPIDY_VERSION}
--- mysql/rompr.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-mysql -u root -e "create database romprdb"
